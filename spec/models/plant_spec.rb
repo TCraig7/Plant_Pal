a36fc47e2f1f186dd98ad71e5950986a20bcd5cf
Tara Craig <tara.craig7@gmail.com>
2018-09-01 17:25:03 -0600

    Add plants controller, plant routes, and view for all plants. ATP.

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Plant, type: :model do
   describe 'validatations' do
-    it {should validate_presence_of(:plant_species)}
+    it {should validate_presence_of(:species)}
     it {should validate_presence_of(:amount_of_water)}
     it {should validate_presence_of(:amount_of_sun)}
   end
