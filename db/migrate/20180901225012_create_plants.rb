e074a085f7ca6c6c51f0ceb8009b7d465bb98e11
Tara Craig <tara.craig7@gmail.com>
2018-09-01 17:17:14 -0600

    Add feature test for user sees all plants.

--- db/migrate/20180901225012_create_plants.rb
+++ db/migrate/20180901225012_create_plants.rb
@@ -1,7 +1,7 @@
 class CreatePlants < ActiveRecord::Migration[5.1]
   def change
     create_table :plants do |t|
-      t.string :plant_species
+      t.string :species
       t.string :nickname
       t.integer :amount_of_water
       t.integer :amount_of_sun
