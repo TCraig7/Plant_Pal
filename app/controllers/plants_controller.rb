8280bdaa7fb41d4d2d8b6786f6409b28833ad41c
Tara Craig <tara.craig7@gmail.com>
2018-09-02 12:38:04 -0600

    Add ability to create a new plant in plant controller. Create new plant form to new view. ATP.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,4 +6,25 @@ class PlantsController < ApplicationController
   def show
     @plant = Plant.find(params[:id])
   end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    plant = Plant.new(plant_params)
+    if plant.save
+      flash[:success] = "#{plant.species} added!"
+      redirect_to plant_path(plant)
+    else
+      flash[:failure] = 'Sorry, please fill out the whole form to create a plant.'
+      redirect_to new_plant_path
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:species, :nickname, :amount_of_water, :amount_of_sun)
+  end
 end
