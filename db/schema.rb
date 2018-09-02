55b91e6317cd7d21551369db8604b515f442ba1b
Tara Craig <tara.craig7@gmail.com>
2018-09-02 16:58:04 -0600

    Add note spec and created db table for notes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901225012) do
+ActiveRecord::Schema.define(version: 20180902225625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.bigint "plant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_notes_on_plant_id"
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "species"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 20180901225012) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notes", "plants"
 end
