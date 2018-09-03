ee3e559d42397e962d0a8c4c1a2dec80931619d6
Tara Craig <tara.craig7@gmail.com>
2018-09-03 15:19:49 -0600

    Add ability to go from category show page to category index page. ATP.

--- spec/features/categories/user_sees_a_single_category_spec.rb
+++ spec/features/categories/user_sees_a_single_category_spec.rb
@@ -12,5 +12,16 @@ describe 'user sees one category' do
 
       expect(current_path).to eq(category_path(category))
     end
+
+    it "can link back to all categories" do
+      plant = Plant.create(species: 'Philodendron Brasil', nickname: 'Steve', amount_of_water: 2, amount_of_sun: 1)
+      category = plant.categories.create(name: "shade loving")
+
+      visit category_path(category)
+
+      click_on "Back to All Categories"
+
+      expect(current_path).to eq(categories_path)
+    end
   end
 end
