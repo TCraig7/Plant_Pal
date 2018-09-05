572b318c25b33fe540f32d7be6d344dea11ffbec
Tara Craig <tara.craig7@gmail.com>
2018-09-04 20:48:50 -0600

    Take away ability for default user to see admin categories. ATP.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,12 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :require_admin
+
   def index
 
   end
+
+  private
+    def require_admin
+      render file: "/public/404" unless current_admin?
+    end
 end
